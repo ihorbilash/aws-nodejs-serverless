26321052b200c6fdf8e3559d301daa9c200f8f47
ihorbilash <first2007.ib@gmail.com>
2023-06-24 13:02:55 +0300

    ..

--- src/functions/cors.ts
+++
@@ -1,7 +0,0 @@
-const corsHeaders = {
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-  };
-  
-  export { corsHeaders };
-  
--- src/functions/user/handler.ts
+++ src/functions/user/handler.ts
@@ -4,19 +4,12 @@ import { middyfy } from "@libs/lambda";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import userService from "src/service";
 import { v4 } from "uuid";
-import { corsHeaders } from '@functions/cors';
 
-const headers = {
-    'Access-Control-Allow-Origin': 'http://44.201.74.140:8080/',
-    'Access-Control-Allow-Credentials': true,
-};
 
 export const getAllUsers = middyfy(async (): Promise<APIGatewayProxyResult> => {
     const users = await userService.getAllUsers();
     return formatJSONResponse({
-        statusCode: 200,
-        headers: { ...headers, ...corsHeaders },
-        body: JSON.stringify({ users }),
+        users
     })
 })
 
@@ -33,15 +26,11 @@ export const createUser = middyfy(async (event: APIGatewayProxyEvent): Promise<A
             createdTime: new Date().toISOString(),
         })
         return formatJSONResponse({
-            statusCode: 200,
-            headers: { ...headers, ...corsHeaders },
-            body: JSON.stringify({ user }),
+            user
         });
     } catch (e) {
         return formatJSONResponse({
-            statusCode: 500,
-            headers: { ...headers, ...corsHeaders },
-            body: JSON.stringify({ status: 500, message: e })
+            status: 500, message: e
         });
     }
 })
@@ -51,15 +40,12 @@ export const updateUser = middyfy(async (event: APIGatewayProxyEvent): Promise<A
     try {
         const user = await userService.updateUser(id, JSON.parse(event.body))
         return formatJSONResponse({
-            statusCode: 200,
-            headers: { ...headers, ...corsHeaders },
-            body: JSON.stringify({ user, id }),
+
+            user, id
         });
     } catch (e) {
         return formatJSONResponse({
-            statusCode: 500,
-            headers: { ...headers, ...corsHeaders },
-            body: JSON.stringify({ status: 500, message: e })
+            status: 500, message: e
         });
     }
 })
@@ -69,15 +55,11 @@ export const deleteUser = middyfy(async (event: APIGatewayProxyEvent): Promise<A
     try {
         const user = await userService.deleteUser(id)
         return formatJSONResponse({
-            statusCode: 200,
-            headers: { ...headers, ...corsHeaders },
-            body: JSON.stringify({ user, id })
+            user, id
         });
     } catch (e) {
         return formatJSONResponse({
-            statusCode: 500,
-            headers: { ...headers, ...corsHeaders },
-            body: JSON.stringify({ status: 500, message: e })
+            status: 500, message: e
         });
     }
 })
